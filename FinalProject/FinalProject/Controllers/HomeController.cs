783b495903ab999c00f18f58102cf7ef57da78b1
J-Macleod <jacob.macleod10@gmail.com>
2022-04-09 22:13:57 -0400

    Created "About Us" Page with Placeholder Text For Description

--- FinalProject/FinalProject/Controllers/HomeController.cs
+++ FinalProject/FinalProject/Controllers/HomeController.cs
@@ -37,6 +37,11 @@ namespace FinalProject.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
